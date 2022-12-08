b477c60dd0668ab27de0062703ad783d9e159f10
Vohizy <hei.vohizy@gmail.com>
2022-12-08 16:57:47 +0300

    chore: delete a comment

--- src/main/java/app/prog/model/Book.java
+++ src/main/java/app/prog/model/Book.java
@@ -19,10 +19,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Book {
-    /*
-    TODO-4-a: Note that i use serial type for ID in database. What does serial do ?
-    TODO-4-b: Should I map it with int ? Fix it if there is a problem
-     */
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "Book_id")
